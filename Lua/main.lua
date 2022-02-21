ea8967af50d30eb2319d061b181580606a207899
14 <100162272+14ql@users.noreply.github.com>
2022-02-21 12:48:12 -0800

    Update main.lua

--- Lua/main.lua
+++ Lua/main.lua
@@ -1,7 +1,10 @@
+local badskid = loadfile('badskid.lua');
+
 local badskid = {};
 
-function badskid.test(e)
-  print(e)
+function badskid.Load(Delay)
+  task.wait(Delay);
+  return true;
 end;
 
 return badskid
