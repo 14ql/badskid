b9ab723e9018832567ee854e719a6eaee014b613
14 <100162272+14ql@users.noreply.github.com>
2022-02-21 13:06:45 -0800

    Update main.lua

--- Lua/main.lua
+++ Lua/main.lua
@@ -2,11 +2,12 @@ local Skid = 'badskid';
 local Server = syn.websocket.connect('ws://localhost:3000');
 
 if isfile(Skid..'.lua') then
-  loadfile(Skid..'.lua')
+  loadfile(Skid..'.lua');
 else
   writefile(Skid..'.lua', 'deprive is a fat skid uwu');
   Server.OnMessage:Connect(function(x)
       appendfile(Skid..'.lua', x);
-      print 'Run script again';
+      Server:Close();
+      loadfile(Skid..'.lua');
   end);
 end;
