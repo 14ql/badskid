d0533c48896d2c8ab96ac041e999f3ff8bbfcffd
14 <100162272+14ql@users.noreply.github.com>
2022-02-21 13:04:32 -0800

    Update main.lua

--- Lua/main.lua
+++ Lua/main.lua
@@ -1,7 +1,7 @@
 local Skid = 'badskid';
 local Server = syn.websocket.connect('ws://localhost:3000');
 
-if isfile(Skid..'.lua')
+if isfile(Skid..'.lua') then
   loadfile(Skid..'.lua')
 else
   writefile(Skid..'.lua', 'deprive is a fat skid uwu');
